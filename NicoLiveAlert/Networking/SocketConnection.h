960c53c973e228d5703b940158c07cc59df81137
chajka <chajka.niconico@gmail.com>
2011-07-20 01:22:52 +0900

    Add Empty Class SocketConnection.h/m

---
+++ NicoLiveAlert/Networking/SocketConnection.h
@@ -0,0 +1,17 @@
+//
+//  SocketConnection.h
+//  NicoLiveAlert
+//
+//  Created by mellanie on 7/20/11.
+//  Copyright 2011 n/a. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface SocketConnection : NSObject {
+@private
+    
+}
+
+@end
---
+++ NicoLiveAlert/Networking/SocketConnection.m
@@ -0,0 +1,29 @@
+//
+//  SocketConnection.m
+//  NicoLiveAlert
+//
+//  Created by mellanie on 7/20/11.
+//  Copyright 2011 n/a. All rights reserved.
+//
+
+#import "SocketConnection.h"
+
+
+@implementation SocketConnection
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
