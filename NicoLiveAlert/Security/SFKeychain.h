df72cff888086011d9045898c77e7c87808e997c
chajka <chajka.niconico@gmail.com>
2011-07-20 01:21:10 +0900

    Add empty class SFKeychain.h/m

---
+++ NicoLiveAlert/Security/SFKeychain.h
@@ -0,0 +1,17 @@
+//
+//  SFKeychain.h
+//  NicoLiveAlert
+//
+//  Created by mellanie on 7/20/11.
+//  Copyright 2011 n/a. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface SFKeychain : NSObject {
+@private
+    
+}
+
+@end
