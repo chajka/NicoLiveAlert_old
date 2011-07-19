99717a44b1495ba182e9efee7d9f4c9854911890
chajka <chajka.niconico@gmail.com>
2011-07-20 01:19:59 +0900

    Add empty Class NicoLive.h/m

---
+++ NicoLiveAlert/NicoLive/NicoLive.h
@@ -0,0 +1,17 @@
+//
+//  NicoLive.h
+//  NicoLiveAlert
+//
+//  Created by mellanie on 7/20/11.
+//  Copyright 2011 n/a. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface NicoLive : NSObject {
+@private
+    
+}
+
+@end
