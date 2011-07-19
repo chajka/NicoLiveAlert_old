266701034eaf1573ef8fa709ebde10bedeb2cf5d
chajka <chajka.niconico@gmail.com>
2011-07-20 01:22:11 +0900

    Add Empty Class HTTPConnection.h/m

---
+++ NicoLiveAlert/Networking/HTTPConnection.m
@@ -0,0 +1,29 @@
+//
+//  HTTPConnection.m
+//  NicoLiveAlert
+//
+//  Created by mellanie on 7/20/11.
+//  Copyright 2011 n/a. All rights reserved.
+//
+
+#import "HTTPConnection.h"
+
+
+@implementation HTTPConnection
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
