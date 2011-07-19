6cb6db054dc28e4f94af0e0d0d6c0019543c378b
chajka <chajka.niconico@gmail.com>
2011-07-20 01:23:48 +0900

    Add Empty Class NicoLiveProgram.h/m

---
+++ NicoLiveAlert/NicoLive/NicoLiveProgram.h
@@ -0,0 +1,17 @@
+//
+//  NicoLiveProgram.h
+//  NicoLiveAlert
+//
+//  Created by mellanie on 7/20/11.
+//  Copyright 2011 n/a. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface NicoLiveProgram : NSObject {
+@private
+    
+}
+
+@end
---
+++ NicoLiveAlert/NicoLive/NicoLiveProgram.m
@@ -0,0 +1,29 @@
+//
+//  NicoLiveProgram.m
+//  NicoLiveAlert
+//
+//  Created by mellanie on 7/20/11.
+//  Copyright 2011 n/a. All rights reserved.
+//
+
+#import "NicoLiveProgram.h"
+
+
+@implementation NicoLiveProgram
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
